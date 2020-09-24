ed4fcb92c3254134610d96b439a7fb40a32ea228
akam alam <arundelfootball23@gmail.com>
2020-09-24 03:36:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(numbers)
  new_number = []
  counter = 0 
  
-  while counter < numbers.length 
+  while counter < numbers.length do 
    new_numbers[counter] = numbers[counter] **2 
   counter += 1
   end 
